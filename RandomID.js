8a63f64a45e2b6c21f0c3a202a0388730010e9d9
inVariabl <danonguitar@gmail.com>
2018-03-18 21:23:55 -0700

    Make it a function!

--- RandomID.js
+++ RandomID.js
@@ -11,6 +11,8 @@
 // CallerID - #address_ua_config_display_name
 // Save Btn - button.btn.btn-default
 
+function random_id(nonrand) {
+
 // Generate Random Phone Number
 var a = Math.floor((Math.random() * 10));
 var b = Math.floor((Math.random() * 10));
@@ -24,10 +26,22 @@ var i = Math.floor((Math.random() * 10));
 var j = Math.floor((Math.random() * 10));
 
 var caller_id = ("1"+ a + b + c + d + e + f + g + h + i + j);
-
-//Change Text-Box of CallerID to generated number
+  
+// If custom #, use it
+if (nonrand.length() >= 0) {
+  var caller_id = nonrand;
+}
+else {
+  console.log("Using randomly generated number...");
+};
+  
+// Change Text-Box of CallerID to generated number
 var phone_input = document.getElementById("address_ua_config_display_name");
   phone_input.value = caller_id;
 
 // Click Save
-$("button.btn.btn-default").click()
+$("button.btn.btn-default").click();
+};
+
+// Run It!
+random_id();
